15c46dbdbb80602600e4c1351e819e8b2adb45a7
Rajesh D <rajeshdunga@hotmail.com>
2015-07-07 19:13:55 +0530

    Add new controller

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 (function(){
 	// Product
-	var gem =  {name:'Azurite', price:2.95};
+	var gems =  [
+				{name:'Azurite', price:2.95, canPurchase:true, soldOut:true},
+				{name:'BloodStone', price:3.95, canPurchase:true, soldOut:false},
+				{name:'Zircon', price:4.00, canPurchase:true, soldOut:false}
+			   ];
 	// Add a new module
 	var app = angular.module('gemStore',[]);
 	// Add a controller for functionality
 	app.controller("StoreController", function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
 })();
