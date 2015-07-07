4651250728adb7bc06bcc856ac9f923c076455e6
Rajesh D <rajeshdunga@hotmail.com>
2015-07-07 14:58:45 +0530

    Add new controller

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var app = angular.module('gemStore',[]);
+(function(){
+	// Product
+	var gem =  {name:'Azurite', price:2.95};
+	// Add a new module
+	var app = angular.module('gemStore',[]);
+	// Add a controller for functionality
+	app.controller("StoreController", function(){
+		this.product = gem;
+	});
+
+})();
