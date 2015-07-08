ab13b825cda51017c886891c6008afe303313db1
Rajesh D <rajeshdunga@hotmail.com>
2015-07-08 19:21:59 +0530

    Add new images

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (function(){
 	// Product
 	var gems =  [
-				{name:'Azurite', price:2.95, canPurchase:true, soldOut:true},
-				{name:'BloodStone', price:3.95, canPurchase:true, soldOut:false},
-				{name:'Zircon', price:4.00, canPurchase:true, soldOut:false}
+					{name:'Azurite', price:2.95, canPurchase:true, soldOut:false, icon: 'images/gem-02.gif'},
+					{name:'BloodStone', price:3.95, canPurchase:true, soldOut:false, icon: 'images/gem-01.gif'},
+					{name:'Zircon', price:4.00, canPurchase:true, soldOut:false, icon: 'images/gem-06.gif'}
 			   ];
 	// Add a new module
 	var app = angular.module('gemStore',[]);
